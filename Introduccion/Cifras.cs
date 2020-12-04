9ffed520fc7c40b5af6c24941331e95936650a5c
Ariel Gallardo <ariel.gallardo96@gmail.com>
2020-12-04 19:57:23 -0300

    Cifras

---
+++ Introduccion/Cifras.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Introduccion
+{
+    class Cifras
+    {
+        private static Int32 Numero { get; set; }
+        static String Determinar() {
+            Int32 longitud = Numero.ToString().Length;
+            switch (longitud)
+            {
+                case 1: case 2: case 3:
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    return $"{longitud} cifras.";
+                default:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    return "El numero de cifras es Mayor";
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Ingresar Numero: ");
+            Numero = Int32.Parse(Console.ReadLine());
+            Console.WriteLine(Determinar());
+            Console.ReadKey();
+        }
+    }
+}
