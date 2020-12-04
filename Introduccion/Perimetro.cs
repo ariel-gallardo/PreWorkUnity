118e7458851aa3b811436dbc73a5e09c7c4da72a
Ariel Gallardo <ariel.gallardo96@gmail.com>
2020-12-04 17:20:41 -0300

    Perimetro

---
+++ Introduccion/Perimetro.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Introduccion
+{
+    class Perimetro
+    {
+        public static double Lado { get; set; }
+        static void Main(string[] args)
+        {
+            Lado = Double.Parse(Console.ReadLine());
+            Lado *= 4;
+            Console.WriteLine("Perimetro: " + Lado);
+            Console.Read();
+        }
+    }
+}
