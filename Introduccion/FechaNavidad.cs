7aab80020e538964132439f9c8de15d2c52b8c14
Ariel Gallardo <ariel.gallardo96@gmail.com>
2020-12-04 20:10:29 -0300

    Fecha Navideña

---
+++ Introduccion/FechaNavidad.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Introduccion
+{
+    class FechaNavidad
+    {
+        static void Main(string[] args)
+        {
+            DateTime fechaActual = new DateTime(Int32.Parse(Console.ReadLine()), Int32.Parse(Console.ReadLine()), Int32.Parse(Console.ReadLine()));
+            Console.WriteLine(fechaActual.ToString("yyyy-MM-dd") + (fechaActual.Day == 25 && fechaActual.Month == 12 ? " es una fecha navideña" : " no es una fecha navideña"));
+            Console.ReadKey();
+        }
+    }
+}
