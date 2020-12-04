d509142404bd5e6acec87d69e038a806e1a83b7f
Ariel Gallardo <ariel.gallardo96@gmail.com>
2020-12-04 19:44:25 -0300

    "Porcentaje y Calificación"

---
+++ Introduccion/SubTotal.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Introduccion
+{
+    class SubTotal
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Precio: ");
+            double precio = Double.Parse(Console.ReadLine());
+            Console.Write("Cantidad: ");
+            double cantidad = Double.Parse(Console.ReadLine());
+            Console.WriteLine($"Precio({precio})  Cantidad({cantidad}) = {precio*cantidad}");
+            Console.ReadKey();
+        }
+    }
+}
