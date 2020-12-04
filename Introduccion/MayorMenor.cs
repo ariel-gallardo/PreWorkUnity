f2ab35b824b8af0dcb350e67848f3e896373e880
Ariel Gallardo <ariel.gallardo96@gmail.com>
2020-12-04 18:32:25 -0300

    PromedioAlumno

---
+++ Introduccion/MayorMenor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Introduccion
+{
+    class MayorMenor
+    {
+        public static void Main(string[] args)
+        {
+            double a = 0, b = 0;
+            Console.Write("A: ");  a = Double.Parse(Console.ReadLine());
+            Console.Write("B: ");  b = Double.Parse(Console.ReadLine());
+            Console.WriteLine(a > b ? $"{a}+{b} = {a+b} | {a}-{b} = {a-b}" : $"{a}*{b} = {a*b} | {a}/{b}={a/b:n}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Introduccion/PromedioAlumno.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Introduccion
+{
+    class PromedioAlumno
+    {
+        public static void Main(string[] args)
+        {
+            double n1 = Double.Parse(Console.ReadLine()), n2 = Double.Parse(Console.ReadLine()), n3 = Double.Parse(Console.ReadLine());
+            double promedio = (n1 + n2 + n3) / 3;
+            if(promedio >= 7)
+            {
+                Console.WriteLine("Aprobado");
+            }
+            Console.ReadKey();
+        }
+    }
+}
