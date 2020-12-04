6a53a2b77e2c4fa254e6427f17ee674a2fa9cb77
Ariel Gallardo <ariel.gallardo96@gmail.com>
2020-12-04 19:09:53 -0300

    Positivo Nulo Negativo

---
+++ Introduccion/PositivoNuloNegativo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Introduccion
+{
+    class PositivoNuloNegativo
+    {
+        static void Main(string[] args)
+        {
+            Double numero = Double.MinValue;
+            try
+            {
+                numero = Double.Parse(Console.ReadLine());
+            }
+            catch (FormatException)
+            {
+                numero = Double.MinValue;
+            }
+            finally
+            {
+                Console.WriteLine(numero == Double.MinValue || numero == 0.0 ? "El valor es nulo" : (numero < 0 ? "Numero Negativo" : "Numero Positivo" ));
+            }
+            Console.ReadKey();
+        }
+    }
+}
