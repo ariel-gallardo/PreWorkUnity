e71189646419694360e4d463e971489d9f745f8b
Ariel Gallardo <ariel.gallardo96@gmail.com>
2020-12-04 20:28:32 -0300

    Cuadrante segun coordenada.

---
+++ Introduccion/Cuadrante.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Introduccion
+{
+    class Coordenada
+    {
+        public Int32 X{ get; set; }
+        public Int32 Y { get; set; }
+
+        public Coordenada()
+        {
+        }
+
+        public Coordenada(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+    class Cuadrante
+    {
+        static Int32 NumCuadrante(ref Coordenada c) {
+            return c.X > 0 && c.Y > 0 ? 1 :
+                c.X < 0 && c.Y > 0 ? 2 :
+                c.X < 0 && c.Y < 0 ? 3 :
+                4;
+        }
+        static void Main(string[] args) {
+            Coordenada c = new Coordenada(Int32.Parse(Console.ReadLine()),Int32.Parse(Console.ReadLine()));
+            Console.WriteLine($"La coordenada ingresada pertenece al cuadrante -> {NumCuadrante(ref c)}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Introduccion/TresValores.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Introduccion
+{
+    class TresValores
+    {
+        static void Main(string[] args)
+        {
+            Int32 numA = Int32.Parse(Console.ReadLine()),
+                numB = Int32.Parse(Console.ReadLine()),
+                numC = Int32.Parse(Console.ReadLine());
+            if(numA == numB && numB == numC)
+            {
+                Console.WriteLine($"({numA}+{numB})*{numC} = {(numA+numB)*numC}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
