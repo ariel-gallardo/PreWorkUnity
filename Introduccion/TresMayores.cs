ca147b7371a7ecc58f18b5c9f3f32a9ec365835f
Ariel Gallardo <ariel.gallardo96@gmail.com>
2020-12-04 18:48:32 -0300

    Tres Mayores

---
+++ Introduccion/TresMayores.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Introduccion
+{
+    class TresMayores
+    {
+        static void Main(string[] args)
+        {
+            double a = Double.Parse(Console.ReadLine()),
+                b = Double.Parse(Console.ReadLine()),
+                c = Double.Parse(Console.ReadLine());
+            Console.WriteLine("\n"+(a>=b && a>=c ? a : b > a && b > c ? b : c));
+            Console.ReadKey();
+        }
+    }
+}
