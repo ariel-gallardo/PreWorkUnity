d5df2cb40e877ed2d3daeffef8048b2d1b12180d
Ariel Gallardo <ariel.gallardo96@gmail.com>
2020-12-05 01:30:55 -0300

    Comparar 2 Listas

---
+++ Introduccion/Listas.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Introduccion
+{
+    class Listas
+    {
+        static Int32[][] Lista = {
+            new Int32[15],
+            new Int32[15] 
+        };
+
+        static void Main(string[] args)
+        {
+            foreach (int[] lista in Lista)
+            {
+                for (int i = 0; i < lista.Count(); i++)
+                {
+                    lista[i] = Int32.Parse(Console.ReadLine());
+                }
+            }
+
+            foreach (int[] list in Lista)
+            {
+                foreach (int value in list)
+                {
+                    Console.Write($"{value} - ");
+                }
+            }
+
+            Console.Clear();
+
+            Console.WriteLine(
+                Lista[0].Max() > Lista[1].Max() ? "Lista 1 Mayor" : Lista[1].Max() == Lista[0].Max() ? "Listas Iguales" : "Lista 2 Mayor"
+            );
+
+            Console.ReadKey();
+        }
+    }
+}
