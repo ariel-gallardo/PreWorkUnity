5fd67d9dfd0df3d2b1ccadb2055b4c8edf796112
Ariel Gallardo <ariel.gallardo96@gmail.com>
2020-12-04 22:28:36 -0300

    Serie del 8

---
+++ Introduccion/Multiplo8.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Introduccion
+{
+    class Multiplo8
+    {
+        static void Main(string[] args)
+        {
+            Int32 num = 0;
+            while (true)
+            {
+                num += 8;
+                if (num > 500) {
+                    break;
+                }
+                else
+                {
+                    Console.Write($"{num} - ");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
