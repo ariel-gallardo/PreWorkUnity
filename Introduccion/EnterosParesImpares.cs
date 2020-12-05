f9a72c81dbdc253d2bd65b28eb01bd980055e3d6
Ariel Gallardo <ariel.gallardo96@gmail.com>
2020-12-05 01:45:33 -0300

    Contar Pares e Impares

---
+++ Introduccion/EnterosParesImpares.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Introduccion
+{
+    class EnterosParesImpares
+    {
+        private static List<Int32>Numeros { get; set; }
+        static void Main(string[] args)
+        {
+            Numeros = new List<Int32>();
+            while (true) {
+                Int32 tempNum = Int32.Parse(Console.ReadLine());
+                if (tempNum == 0) break;
+                Numeros.Add(tempNum);
+            }
+            Console.WriteLine($"Cantidad -> Pares({Numeros.Count(n => n % 2 == 0)}) | Impares({Numeros.Count(n => n%2 == 1)})");
+            Console.ReadKey();
+        }
+    }
+}
