d3dc2ae2d9df23154a20bcb51ad2f47002dab9c2
Ariel Gallardo <ariel.gallardo96@gmail.com>
2020-12-04 22:21:23 -0300

    25 Num serie 11

---
+++ Introduccion/Serie25.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Introduccion
+{
+    class Serie25
+    {
+        static void Main(string[] args)
+        {
+            int num = 0;
+            for (int i = 0; i < 25; i++)
+            {
+                Console.WriteLine($"{num+=11}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
