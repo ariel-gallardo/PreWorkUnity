57c52cafc96abe7a3ab5b807bef0c71a05b1561b
Ariel Gallardo <ariel.gallardo96@gmail.com>
2020-12-04 22:03:20 -0300

    Promedio de Alturas

---
+++ Introduccion/AlturaPromedio.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Introduccion
+{
+    class AlturaPromedio
+    {
+        private static List<Double> Alturas { get; set; }
+        static void Main(string[] args)
+        {
+            Alturas = new List<Double>();
+            Console.WriteLine("Ingresar numero con formato de , no con . ");
+            while (true)
+            {
+                Console.Write($"Altura n° {Alturas.Count+1}: ");
+                double altura = double.Parse(Console.ReadLine());
+                if (altura <= 0.0) { break; }
+                Alturas.Add(altura);
+            }
+            Console.WriteLine($"Total de personas {Alturas.Count} con un promedio: {(Alturas.Sum()/Alturas.Count):n}");
+            Console.ReadKey();
+        }
+    }
+}
