56ce7ecbcf69307234e2cdc7a922ba86d562b798
Ariel Gallardo <ariel.gallardo96@gmail.com>
2020-12-05 03:27:38 -0300

    Numero menor repetido

---
+++ Introduccion/MenorRepetido.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Introduccion
+{
+	class MenorRepetido
+	{
+		static void Main(string[] args)
+		{
+			List<Int32> Numeros = new List<int>() {
+				1,
+				1,
+				3,
+				5,
+				6,
+				3,
+				8,
+				3
+			};
+			Console.WriteLine($"Numero menor -> {Numeros.Min()} {(Numeros.Count(num => num == Numeros.Min()) > 1 ? "si" : "no")} se repite");
+			Console.ReadKey();
+		}
+	}
+}
