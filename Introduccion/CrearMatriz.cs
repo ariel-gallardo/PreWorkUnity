89f80d48d10e0bd98ce8ea7e8bd78e2a7d919b5b
Ariel Gallardo <ariel.gallardo96@gmail.com>
2020-12-05 13:46:27 -0300

    Crear Matriz 2D

---
+++ Introduccion/CrearMatriz.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Introduccion
+{
+	class CrearMatriz
+	{
+		static void Main(string[] args)
+		{
+
+			int[,] Matriz = new int[2, 5];
+			for (int i = 0; i < Matriz.Rank; i++)
+			{
+				for (int j = 0; j < Matriz.Length/Matriz.Rank; j++)
+				{
+					Matriz[i, j] = Int32.Parse(Console.ReadLine());
+				}
+			}
+
+			foreach (var item in Matriz)
+			{
+				Console.Write($"{item} ");
+			}
+
+			Console.ReadKey();
+		}
+	}
+}
